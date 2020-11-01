5740ba3ea33ea0067eb2b270be83cf127184f97f
Maxim <SokolovMN123@yandex.ru>
2020-11-01 13:47:05 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Flask YouTube search
+
+Программа поработает с API YouTube через web-интерфейс и показывает 12 лучших результатов по поисковому запросу.
+
+Функционал:
+
+- 12 первых результатов поиска по поисковому запросу;
+- Случайный результат по запросу;
+
+![Flask YouTube search](http://gamedz.ru/wp-content/uploads/2020/youtube.gif "Flask YouTube search")
+
+## Зависимости
+- Flask
+- YouTube API
+- Requests
+- python-dotenv
+
+## Установка
+
+Установка виртуального окружения
+
+`python -m venv venv`
+
+Установка зависимостей
+
+`pip install -r requirements.txt`
+
+## Запуск программы
+
+`flask run`
---
+++ flask_youtube_search/routes.py
@@ -0,0 +1,58 @@
+import requests
+from isodate import parse_duration
+from flask import Blueprint, render_template, current_app, request, redirect
+import random
+
+main = Blueprint('main', __name__)
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
+    videos = []
+
+    if request.method == 'POST':
+
+        search_params = {
+            'key': current_app.config['YOUTUBE_API_KEY'],
+            'q': request.form.get('query'),
+            'part': 'snippet',
+            'maxResults': 12,
+            'type': 'video'
+        }
+
+        r = requests.get(search_url, params=search_params)
+
+        video_ids = []
+        results = r.json()['items']
+
+        for result in results:
+            video_ids.append(result['id']['videoId'])
+
+        if request.form.get('submit') == 'lucky':
+            return redirect(f'https://www.youtube.com/watch?v={ video_ids[random.randrange(0, len(video_ids) -1)]}')
+
+        video_params = {
+            'key': current_app.config['YOUTUBE_API_KEY'],
+            'id': ','.join(video_ids),
+            'part': 'snippet,contentDetails',
+            'maxResults': 12,
+            'type': 'video'
+        }
+
+        r = requests.get(video_url, params=video_params)
+
+        results = r.json()['items']
+
+        
+        for result in results:
+            video_data = {
+                'id': result['id'],
+                'url': f'https://www.youtube.com/watch?v={ result["id"] }',
+                'thumbnail': result['snippet']['thumbnails']['high']['url'],
+                'duration': int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
+                'title': result['snippet']['title']
+            }
+            videos.append(video_data)
+
+    return render_template('index.html', videos=videos)
